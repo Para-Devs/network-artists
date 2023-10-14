89369dfbd6938120de49d21888124cc1f9356ad4
Kerschbaumer Stefan <xsip@pm.me>
2023-10-14 11:01:14 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,21 @@ import {TranslateModule} from '@ngx-translate/core';
   standalone: true,
   imports: [CommonModule, MenuBurgerComponent, ContactFormComponent, TranslateModule],
   template: `
-    <app-menu-burger></app-menu-burger>
-    <app-contact-form></app-contact-form>
-    <h1 class="text-3xl font-bold underline">
-      {{'welcome' | translate}}
-    </h1>
+      <section class="w-full h-full">
+          <app-menu-burger></app-menu-burger>
+          <app-contact-form></app-contact-form>
+          <h1 class="text-3xl font-bold underline">
+              {{'welcome' | translate}}
+          </h1>
+      </section>
+      <section class="w-full h-full bg-slate-800 flex">
+        <div class="md:w-[40%] md:h-full">
+
+        </div>
+        <div class="md:w-[60%] md:h-full">
+          <img alt="placeholder" class="md:h-full" src="../assets/img/placeholder.png"/>
+        </div>
+      </section>
   `,
   styles: [],
 })
