43d1e08311d36122a23eea381c0a4ad3cce1ca84
Kerschbaumer Stefan <xsip@pm.me>
2023-10-14 10:39:21 +0200

    adds ngx translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MenuBurgerComponent} from './menu-burger/menu-burger.component';
 import {ContactFormComponent} from './forms/contact-form/contact-form.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MenuBurgerComponent, ContactFormComponent],
+  imports: [CommonModule, MenuBurgerComponent, ContactFormComponent, TranslateModule],
   template: `
     <app-menu-burger></app-menu-burger>
     <app-contact-form></app-contact-form>
     <h1 class="text-3xl font-bold underline">
-      Landing
+      {{'welcome' | translate}}
     </h1>
   `,
   styles: [],
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    importProvidersFrom(
+
+      HttpClientModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient],
+        }
+      })
+    )
+  ]
 };
