4a6b38b1bbbbb754f741c16c966aadbbd8ebe333
Kerschbaumer Stefan <xsip@pm.me>
2023-11-17 16:27:07 +0100

    adds ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
@@ -19,6 +20,6 @@ export const appConfig: ApplicationConfig = {
           deps: [HttpClient],
         }
       })
-    )
+    ), provideClientHydration()
   ]
 };
