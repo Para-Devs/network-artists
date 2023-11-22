4e5686ca822799e892683d415f5e8d53dc47d07f
Kerschbaumer Stefan <xsip@pm.me>
2023-11-22 16:14:22 +0100

    adds new logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ function fadeInOut(fadeInTime = '1s', fadeOutTime = '1s') {
                 class="rounded border-[0.5px] border-neutral-50 px-5 mt-5 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 animate-bounce "
                 data-te-ripple-init
                 data-te-ripple-color="light">
-                Contact
+                <a href="mailto:lj@network-artists.com">{{'contact' | translate}}</a>
               </button>
             </div>
           </div>
