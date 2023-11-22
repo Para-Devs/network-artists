52dfeb194cd049b2f6ecf7a41b43228a379a3b59
Kerschbaumer Stefan <xsip@pm.me>
2023-11-22 16:10:00 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,19 +35,19 @@ function fadeInOut(fadeInTime = '1s', fadeOutTime = '1s') {
       <div class="absolute overflow-hidden bg-cover bg-center bg-no-repeat p-12 text-center h-full w-full" style="background-image: url('../assets/img/networkArtistBackGround.png');">
         <div [@fadeInOut]="true"
           class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
-          <div class="flex h-full items-center justify-center ">
-            <div class="text-white bg-[rgba(255,255,255,0.2)] p-10 rounded">
+          <div class="flex h-full md:items-center md:justify-center ">
+            <div class="text-white md:w-auto w-full md:bg-[rgba(255,255,255,0.2)] bg-[rgba(255,255,255,0.08)] p-10 rounded">
               <div class="logo">
                 <img class="svg" src="../assets/img/Network_Artist_Logo_white.svg" alt="test" >
               </div>
-              <h1 class="mb-6 mt-6 text-xl font-sans uppercase tracking-tighter">coming soon ...</h1>
+              <h1 class="mb-6 mt-6 text-xl test uppercase tracking-tighter">coming soon ...</h1>
 
               <button
                 type="button"
-                class="rounded border-2 border-neutral-50 px-5 mt-5 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 animate-bounce "
+                class="rounded border-[0.5px] border-neutral-50 px-5 mt-5 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 animate-bounce "
                 data-te-ripple-init
                 data-te-ripple-color="light">
-                Call to action
+                Contact
               </button>
             </div>
           </div>
@@ -76,6 +76,7 @@ function fadeInOut(fadeInTime = '1s', fadeOutTime = '1s') {
           display:none
         }
       }
+
     `
   ],
   animations: [fadeInOut('2s')]
