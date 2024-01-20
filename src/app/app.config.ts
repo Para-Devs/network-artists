f7c081e0884369eff6fa2b9553c67fbe9611ee0c
Kerschbaumer Stefan <xsip@pm.me>
2024-01-20 20:43:35 +0100

    adds routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideRouter, withDebugTracing} from '@angular/router';
+import {appRoutes} from './app.routes';
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(appRoutes,withDebugTracing()),
     importProvidersFrom(
       BrowserAnimationsModule,
       HttpClientModule,
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import {Routes} from '@angular/router';
+import {ConnectComponent} from './routes/connect/connect.component';
+import {HomeComponent} from './routes/home/home.component';
+import {MethodComponent} from './routes/method/method.component';
+import {MissionComponent} from './routes/mission/mission.component';
+import {ServiceComponent} from './routes/service/service.component';
+import {VisionComponent} from './routes/vision/vision.component';
+
+export const appRoutes: Routes = [
+  {path: '*', redirectTo: 'home' },
+  {path: 'home', pathMatch: 'full', component: HomeComponent },
+  {path: 'connect', pathMatch: 'full', component: ConnectComponent},
+  {path: 'method', pathMatch: 'full', component: MethodComponent},
+  {path: 'mission', pathMatch: 'full', component: MissionComponent},
+  {path: 'service', pathMatch: 'full', component: ServiceComponent},
+  {path: 'vision', pathMatch: 'full', component: VisionComponent}
+]
