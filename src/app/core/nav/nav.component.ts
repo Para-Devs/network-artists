3b9f6893778ef39c70a4f7e63ccdc3aa65fe4ab9
Kerschbaumer Stefan <xsip@pm.me>
2024-01-20 20:35:27 +0100

    adds nav and footer

---
+++ src/app/core/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      nav works!
+    </p>
+  `,
+  styles: ``
+})
+export class NavComponent {
+
+}
