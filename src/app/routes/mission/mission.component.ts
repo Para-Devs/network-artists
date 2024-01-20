7dabdb909b5bbd38cd2b1ef0515078c4f639504b
Kerschbaumer Stefan <xsip@pm.me>
2024-01-20 21:08:54 +0100

    adds mission text

--- src/app/routes/mission/mission.component.ts
+++ src/app/routes/mission/mission.component.ts
@@ -6,9 +6,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      mission works!
-    </p>
+      <section class="w-full h-full flex  items-center flex-col">
+          <div class="mx-40">
+              <h1 class="text-center mt-40 leading-[100px] text-white uppercase text-[130px]">Our mission<br>Is to
+                  connect</h1>
+              <h1 class="text-center mt-20 leading-[90px] p-10 text-white uppercase text-[130px] border-2 border-white">
+                  The dots.</h1>
+          </div>
+      </section>
   `,
   styles: ``
 })
