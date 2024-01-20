6030fdd14575cb80b4c67e5be4d4b2c1b1869eac
Kerschbaumer Stefan <xsip@pm.me>
2024-01-20 20:38:02 +0100

    adds routes

---
+++ src/app/routes/mission/mission.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mission',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      mission works!
+    </p>
+  `,
+  styles: ``
+})
+export class MissionComponent {
+
+}
---
+++ src/app/routes/vision/vision.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-vision',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      vision works!
+    </p>
+  `,
+  styles: ``
+})
+export class VisionComponent {
+
+}
