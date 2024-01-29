59df103350fb563ad7078e18df9088ed9994dc61
leoavram <leo.avramovic@gmail.com>
2024-01-29 20:37:48 +0100

    play with mission background circle animation ( nur a idee )

--- src/app/routes/mission/mission.component.ts
+++ src/app/routes/mission/mission.component.ts
@@ -12,9 +12,58 @@ import {fadeInOut} from '../../animations';
                   connect</h1>
               <h2 class="text-center mt-20  leading-[30px] md:leading-[90px] p-10 text-white uppercase  text-[40px] md:text-[130px] border-2 border-white font-extralight">
                   The dots.</h2>
+
+<!--        play with circle animation -->
+        <div class="main">
+          <div class="circle"></div>
+        </div>
       </section>
   `,
   styles: [`
+
+    .main {
+      -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
+      filter: blur(2px);
+      width: 330px;
+      height: 330px;
+      border: 1px solid rgba(255, 255, 255, 0.30);
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      border-radius: 50%;
+    }
+
+    .circle {
+      -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
+      filter: blur(5px);
+      width: 30px;
+      height: 30px;
+      background: rgba(255, 255, 255, 0.30);
+      border-radius: 50%;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      overflow: hidden;
+      margin: auto;
+      animation: circle 8s linear infinite;
+
+    }
+
+    @keyframes circle {
+      0% {
+        transform: rotate(0deg) translate(-165px) rotate(0deg);
+
+      }
+      100% {
+        transform: rotate(360deg) translate(-165px) rotate(-360deg);
+      }
+    }
+
 
   `],
   animations: [fadeInOut('4s')]
